4b203f36a4d18659913b8c315b6ce1c6372230bd
kivinju <kevinkaizhou@gmail.com>
2023-06-11 16:31:25 -0700

    fix mov as script problem

--- vite.config.js
+++ vite.config.js
@@ -11,9 +11,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  build: {
-    rollupOptions: {
-      external: ['/om_prototype.mov'],
-    }
-  }
+  assetsInclude: ['**/*.mov'],
 })
